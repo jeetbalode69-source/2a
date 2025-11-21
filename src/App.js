0577048ecdb7bb5d905dd2430ba1fc52f83de381
jeetbalode69-source <jeetbalode69@gmail.com>
2025-11-21 17:17:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // App.js (React) updated title
-import React, { useState } from 'react';
+import campusCravingsLogo from './campus-cravings.jpg';  // Adjust path if it's inside assets
+
+//import React, { useState } from 'react';
 
 const foodMenu = [
   { id: 1, name: "Veg Burger", category: "Snacks", price: 50 },
